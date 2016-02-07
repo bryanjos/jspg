938491d1b2ce79019fd812e7c3955e0dd1129cde
Bryan Joseph <bryanjos@gmail.com>
2016-02-06 20:49:41 -0600

    Add web project templates. Renamed to jspg

--- README.md
+++ README.md
@@ -28,32 +28,24 @@ Projects use npm to run scripts above:
 
 These are the defaults. If one wanted to change the tool used, the idea would be to:
 1. Add the tool to the project
-2. Update the existing npm script to use new tool (Or create new npm script if new functionality)
+2. Update the existing npm script to use new tool (or create new npm script if new functionality)
 
 This allows for the commands to stay the same, but the functionality to change.
 
 ## Structure
 The structure of projects is defined below:
 
-`src/` - The src folder. This is where you would write your code
-`src/index.js` - The entry point to your application.
-`test/` - The test folder. All of your tests would go here
-`test/index-test.js` - An example test for `src/index.js`
-`.babelrc` - The babel configuration file
-`.gitignore`
-`package.json`
-`webpack.config.js` - The webpack configuration
+* `src/` - The src folder. This is where you would write your code
+* `src/index.js` - The entry point to your application.
+* `test/` - The test folder. All of your tests would go here
+* `test/index-test.js` - An example test for `src/index.js`
+* `.babelrc` - The babel configuration file
+* `.gitignore`
+* `package.json`
+* `webpack.config.js` - The webpack configuration
+* `build/` - Build ends up here when `npm run build` is executed
+* `docs/` - Documentation ends up here when `npm run docs` is executed
 
-When `npm run build` is executed, code ends up in `build/` folder
-When `npm run docs` is executed, documentation ends up in `docs/` folder
-
-## FAQ
-
-### Why Standard?
-Because it's a "standard" JavaScript code style and removes the need to configure a linter yourself
-
-### Why Webpack?
-It is a popular, powerful and extremely configurable tool for building assets.
-
-### Why Mocha?
-It is a popular testing framework
+Web projects include the following:
+* `src/index.html` - The starting html page
+* `src/styles/app.css` - The styles for your application
--- src/index.js
+++ src/index.js
@@ -4,33 +4,62 @@ var fs = require('fs')
 function create (app_path, isWebProject) {
   var app_name = path.basename(app_path)
   app_path = app_path.charAt(app_path.length - 1) === "/" ? app_path.substring(0, app_path.length - 1) : app_path
-  var target = isWebProject ? "web" : "node"
 
   fs.mkdirSync(app_path)
   fs.mkdirSync(app_path + "/src")
   fs.mkdirSync(app_path + "/test")
 
-
-  var package_json_template = fs.readFileSync(__dirname + '/templates/package.json.template', 'utf8')
-  package_json_template = package_json_template.replace("{{app_name}}", app_name)
-  fs.writeFileSync(app_path + "/package.json", package_json_template)
-
   var gitignore_template = fs.readFileSync(__dirname + '/templates/.gitignore.template', 'utf8')
   fs.writeFileSync(app_path + "/.gitignore", gitignore_template)
-
-  var webpack_template = fs.readFileSync(__dirname + '/templates/webpack.config.js.template', 'utf8')
-  webpack_template = webpack_template
-    .replace("{{target}}", target)
-  fs.writeFileSync(app_path + "/webpack.config.js", webpack_template)
 
   var babelrc_template = fs.readFileSync(__dirname + '/templates/.babelrc.template', 'utf8')
   fs.writeFileSync(app_path + "/.babelrc", babelrc_template)
 
   var index_template = fs.readFileSync(__dirname + '/templates/index.js.template', 'utf8')
+
+  if(isWebProject){
+    index_template = index_template.replace("{{style}}", "require('./styles.css')\nif (process.env.NODE_ENV !== 'production') { require('./index.html') }")
+  }else{
+    index_template = index_template.replace("{{style}}", "")
+  }
+
   fs.writeFileSync(app_path + "/src/index.js", index_template)
 
   var index_test_template = fs.readFileSync(__dirname + '/templates/index-test.js.template', 'utf8')
   fs.writeFileSync(app_path + "/test/index-test.js", index_test_template)
+
+
+  if(isWebProject){
+
+    var package_json_template = fs.readFileSync(__dirname + '/templates/web/package.json.template', 'utf8')
+    package_json_template = package_json_template.replace("{{app_name}}", app_name)
+    fs.writeFileSync(app_path + "/package.json", package_json_template)
+
+    var webpack_dev_template = fs.readFileSync(__dirname + '/templates/web/webpack.config.dev.js.template', 'utf8')
+    fs.writeFileSync(app_path + "/webpack.config.dev.js", webpack_dev_template)
+
+    var webpack_prod_template = fs.readFileSync(__dirname + '/templates/web/webpack.config.prod.js.template', 'utf8')
+    fs.writeFileSync(app_path + "/webpack.config.prod.js", webpack_prod_template)
+
+    var index_html_template = fs.readFileSync(__dirname + '/templates/web/index.html.template', 'utf8')
+    index_html_template = index_html_template.replace("{{app_name}}", app_name)
+    fs.writeFileSync(app_path + "/src/index.html", index_html_template)
+
+    var styles_template = fs.readFileSync(__dirname + '/templates/web/styles.css.template', 'utf8')
+    fs.writeFileSync(app_path + "/src/styles.css", styles_template)
+
+
+  }else{
+
+    var package_json_template = fs.readFileSync(__dirname + '/templates/node/package.json.template', 'utf8')
+    package_json_template = package_json_template.replace("{{app_name}}", app_name)
+    fs.writeFileSync(app_path + "/package.json", package_json_template)
+
+
+    var webpack_template = fs.readFileSync(__dirname + '/templates/node/webpack.config.js.template', 'utf8')
+    fs.writeFileSync(app_path + "/webpack.config.js", webpack_template)
+
+  }
 }
 
 module.exports = {
