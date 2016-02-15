7798104e1daf73859d3627fa3177e9ef7e4e6cef
Bryan Joseph <bryanjos@gmail.com>
2016-02-15 09:24:29 -0600

    Added handlebars for inputting data into templates

--- src/index.js
+++ src/index.js
@@ -1,71 +1,60 @@
 var path = require('path')
 var fs = require('fs')
+var handlebars = require('handlebars')
 
-function create (app_path, isWebProject) {
-  var app_name = path.basename(app_path)
-  app_path = app_path.charAt(app_path.length - 1) === "/" ? app_path.substring(0, app_path.length - 1) : app_path
-
-  fs.mkdirSync(app_path)
-  fs.mkdirSync(app_path + "/app")
-  fs.mkdirSync(app_path + "/app/" + app_name)
-  fs.mkdirSync(app_path + "/tests")
-
-  var gitignore_template = fs.readFileSync(__dirname + '/templates/.gitignore.template', 'utf8')
-  fs.writeFileSync(app_path + "/.gitignore", gitignore_template)
-
-  var babelrc_template = fs.readFileSync(__dirname + '/templates/.babelrc.template', 'utf8')
-  fs.writeFileSync(app_path + "/.babelrc", babelrc_template)
-
-  var app_template = fs.readFileSync(__dirname + '/templates/app.js.template', 'utf8')
-
-  if(isWebProject){
-    app_template = app_template.replace("{{style}}", "require('./" + app_name + "/styles.css')\nif (process.env.NODE_ENV !== 'production') { require('./index.html') }")
-  }else{
-    app_template = app_template.replace("{{style}}", "")
+function removeUnderscore (appPath) {
+  if(appPath.charAt(appPath.length -1) === '/') {
+    return appPath.substring(0, appPath.length - 1)
   }
 
-  fs.writeFileSync(app_path + "/app/app.js", app_template)
+  return appPath
+}
 
-  var app_test_template = fs.readFileSync(__dirname + '/templates/app-test.js.template', 'utf8')
-  fs.writeFileSync(app_path + "/tests/app-test.js", app_test_template)
+function applyTemplate (inputPath, outputPath, data) {
+  var data = data || {}
+  var text = fs.readFileSync(inputPath, 'utf8')
+  var template = handlebars.compile(text)
+  var result = template(data)
+  fs.writeFileSync(outputPath, result)
+}
 
+function create (appPath, isWebProject) {
+  var appName = path.basename(appPath)
+  appPath = removeUnderscore(appPath)
+
+  fs.mkdirSync(appPath)
+  fs.mkdirSync(appPath + "/app")
+  fs.mkdirSync(appPath + "/app/" + appName)
+  fs.mkdirSync(appPath + "/tests")
+
+  applyTemplate(__dirname + '/templates/.gitignore', appPath + "/.gitignore")
+  applyTemplate(__dirname + '/templates/.babelrc', appPath + "/.babelrc")
+  applyTemplate(__dirname + '/templates/app-test.js', appPath + "/tests/app-test.js")
 
   if(isWebProject){
 
-    var package_json_template = fs.readFileSync(__dirname + '/templates/web/package.json.template', 'utf8')
-    package_json_template = package_json_template.replace("{{app_name}}", app_name)
-    fs.writeFileSync(app_path + "/package.json", package_json_template)
+    var styleRequire = "require('./" + appName + "/styles.css')\nif (process.env.NODE_ENV !== 'production') { require('./index.html') }"
 
-    var webpack_base_template = fs.readFileSync(__dirname + '/templates/web/webpack.config.base.js.template', 'utf8')
-    fs.writeFileSync(app_path + "/webpack.config.base.js", webpack_base_template)
+    applyTemplate(__dirname + '/templates/app.js', appPath + "/app/app.js", {"style": styleRequire})
 
-    var webpack_dev_template = fs.readFileSync(__dirname + '/templates/web/webpack.config.dev.js.template', 'utf8')
-    fs.writeFileSync(app_path + "/webpack.config.dev.js", webpack_dev_template)
+    applyTemplate(__dirname + '/templates/web/package.json', appPath + "/package.json", {"appName": appName})
+    applyTemplate(__dirname + '/templates/web/index.html', appPath + "/app/index.html", {"appName": appName})
+    applyTemplate(__dirname + '/templates/web/styles.css', appPath + "/app/" + appName + "/styles.css")
 
-    var webpack_prod_template = fs.readFileSync(__dirname + '/templates/web/webpack.config.prod.js.template', 'utf8')
-    fs.writeFileSync(app_path + "/webpack.config.prod.js", webpack_prod_template)
 
-    var index_html_template = fs.readFileSync(__dirname + '/templates/web/index.html.template', 'utf8')
-    index_html_template = index_html_template.replace("{{app_name}}", app_name)
-    fs.writeFileSync(app_path + "/app/index.html", index_html_template)
-
-    var styles_template = fs.readFileSync(__dirname + '/templates/web/styles.css.template', 'utf8')
-    fs.writeFileSync(app_path + "/app/" + app_name + "/styles.css", styles_template)
-
+    applyTemplate(__dirname + '/templates/web/webpack.config.base.js', appPath + "/webpack.config.base.js")
+    applyTemplate(__dirname + '/templates/web/webpack.config.dev.js', appPath + "/webpack.config.dev.js")
+    applyTemplate(__dirname + '/templates/web/webpack.config.prod.js', appPath + "/webpack.config.prod.js")
 
   }else{
 
-    var package_json_template = fs.readFileSync(__dirname + '/templates/node/package.json.template', 'utf8')
-    package_json_template = package_json_template.replace("{{app_name}}", app_name)
-    fs.writeFileSync(app_path + "/package.json", package_json_template)
-
-
-    var webpack_template = fs.readFileSync(__dirname + '/templates/node/webpack.config.js.template', 'utf8')
-    fs.writeFileSync(app_path + "/webpack.config.js", webpack_template)
+    applyTemplate(__dirname + '/templates/app.js', appPath + "/app/app.js", {"style": ""})
+    applyTemplate(__dirname + '/templates/node/package.json', appPath + "/package.json", {"appName": appName})
+    applyTemplate(__dirname + '/templates/node/webpack.config.js', appPath + "/webpack.config.js")
 
   }
 
-  console.log("Project created at " + path.resolve(app_path))
+  console.log("Project created at " + path.resolve(appPath))
   console.log("Make sure to run 'npm install'")
 }
 
